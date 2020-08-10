cbf542b365198f9eba318435e706ca1bc0b705f4
Dahee <hoihee00@naver.com>
2020-08-10 12:15:03 +0900

    second commit

---
+++ VariableExample.java
@@ -0,0 +1,8 @@
+piblic calss VariableExample {
+	piblic static void main(String[] args) {
+		int value1 = 10;
+		int value1 = 20;
+		int result = 10 + 20;
+		System.out.printin("10 + 20 = " + result);
+	}
+}
